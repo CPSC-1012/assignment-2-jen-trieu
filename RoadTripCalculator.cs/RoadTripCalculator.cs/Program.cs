4dd1e4efbd6d729a1a2c2c8236e2ec32f103942d
Jenny TRIEU <jtrieu7@nait.ca>
2026-03-04 20:57:15 -0700

    Summary Block

---
+++ RoadTripCalculator.cs/RoadTripCalculator.cs/Program.cs
@@ -0,0 +1,21 @@
+﻿///<summary>
+/// RoadTripCalculator.cs
+/// Description : Program calculates the total cost of a road trip based on distance, vehicle fuel efficiency, gas price, and snack costs, and allows the user to repeat the calculation.
+/// Author : Jenny Trieu
+/// Date : March 03 2026
+///</summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadTripCalculator.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
